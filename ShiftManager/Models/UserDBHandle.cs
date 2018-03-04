7d0224701dc6504a85efafc6f18a90c6a1317b08
Alex Wainshtein <w.alex2k@gmail.com>
2018-03-05 00:46:35 +0200

    Started the shift manager again from the ground up to use sql instead of EF6.

---
+++ ShiftManager/Models/UserDBHandle.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+
+namespace ShiftManager.Models
+{
+    public class UserDBHandle
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["SystemDataConn"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        public bool AddUser(UserModel model)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("AddNewUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@FirstName", model.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", model.LastName);
+            cmd.Parameters.AddWithValue("@Email", model.Email);
+            cmd.Parameters.AddWithValue("@IdCard", model.IdCard);
+            cmd.Parameters.AddWithValue("@RoleId", model.RoleId);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+
+        public bool UpdateDetails(UserModel model)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("EditUserDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@UsrId", model.Id);
+            cmd.Parameters.AddWithValue("@FirstName", model.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", model.LastName);
+            cmd.Parameters.AddWithValue("@Email", model.Email);
+            cmd.Parameters.AddWithValue("@IdCard", model.IdCard);
+            cmd.Parameters.AddWithValue("@RoleId", model.RoleId);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        public bool DeleteUser(int id)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("DeleteUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@UsrId", id);
+            
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+
+        public List<UserModel> GetUser()
+        {
+            connection();
+            List<UserModel> userList = new List<UserModel>();
+
+            SqlCommand cmd = new SqlCommand("GetUserDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                userList.Add(
+                    new UserModel
+                    {
+                        Id = Convert.ToInt32(dr["Id"]),
+                        FirstName = Convert.ToString(dr["FirstName"]),
+                        LastName = Convert.ToString(dr["LastName"]),
+                        Email = Convert.ToString(dr["Email"]),
+                        IdCard = Convert.ToString(dr["IdCard"]),
+                        RoleId = Convert.ToInt32(dr["RoleId"])
+
+
+                    });
+            }
+            return userList;
+        }
+
+    }
+}
